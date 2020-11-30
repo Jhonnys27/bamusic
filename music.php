14dd5ce4fa7237a6e48aa7e66ce19c38e52537b1
Jhonnys27 <61165176+Jhonnys27@users.noreply.github.com>
2020-11-30 00:27:08 -0500

    Add files via upload

---
+++ music.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/enca.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="shortcut icon" href="img/icono.png">
+    <title>BAMUSIC</title>
+</head>
+<body>
+<br>
+<ul class="menu">
+			<li><a>BAMUSIC</a></li>
+            <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfwmHj-UrcvwXZObbINjbGovuRL2FUG8iml6BkzaCnsScD0-w/viewform?usp=sf_link">Llenar encuesta</a></li>
+            
+</ul>
+<br>
+<h1 align="center">Álbum de musica</h1>
+
+
+<div class="container">
+    <div class="item">
+        <center>
+      <img src="img/johann.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Prelude in G - Major</h3>
+        <p>Autor: Johann Sebastian Bach<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Johann Sebastian Bach - Cello suite No.1 Prelude in G - Major.mp3" class="audio" controls  title="Eraser" download"Bienvenida.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/mozart.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Turkish March</h3>
+        <p>Autor: Wolfgang Amadeus Mozart<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Turkish March.mp3" class="audio" controls  title="Eraser" download"TurkishMarch.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/Beethoven.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Moonlight</h3>
+        <p>Autor: Ludwig van Beethoven<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Moonlight.mp3" class="audio" controls  title="Eraser" download"TurkishMarch.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/mozart.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Lacrimosa</h3>
+        <p>Autor: Wolfgang Amadeus Mozart<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Lacrimosa.mp3" class="audio" controls  title="Eraser" download"TurkishMarch.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/Chopin.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Ballade No 1</h3>
+        <p>Autor: Frédéric Chopin<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/ballade.mp3" class="audio" controls  title="Eraser" download"Bienvenida.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/Tschaikowski.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Waltz of the Flowers</h3>
+        <p>Autor: Piotr Ilich Chaikovski<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Waltz of the Flowers.mp3" class="audio" controls  title="Eraser" download"Bienvenida.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/haendel.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Sarabande</h3>
+        <p>Autor: Georg Friedrich Händel<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Sarabande.mp3" class="audio" controls  title="Eraser" download"Bienvenida.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/Beethoven.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Para Elisa</h3>
+        <p>Autor: Ludwig van Beethoven<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Para Elisa.mp3" class="audio" controls  title="Eraser" download"TurkishMarch.mp3"></audio>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+        <center>
+      <img src="img/Vivaldi.jpg" alt="" class="item-img" width="250" height="250">
+        </center>
+      <div class="item-text">
+        <h3>Storm</h3>
+        <p>Autor: Antonio Vivaldi<br>Tipo: Barroca</p>
+        <div class="panel-footer">
+        <audio src="audios/Storm.mp3" class="audio" controls  title="Eraser" download"TurkishMarch.mp3"></audio>
+        </div>
+      </div>
+    </div>
+    </div>
+    <footer>
+    jaskdd
+    </footer>    
+</body>
+</html>
+
